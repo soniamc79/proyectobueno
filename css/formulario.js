b6da75688d5135baf86051e8fdd417b64bc9e4cc
SONIA MC <sonia.martinezcalleja@mengibar.eu>
2023-01-04 17:41:50 +0100

    formulario
    sigo con las validaciones del fomulario

--- css/formulario.js
+++ css/formulario.js
@@ -1,4 +1,18 @@
-//objeto con las normas de cada div
-const expresiones={
-    nombre: /
+
+var b = document.getElementById("enviar");
+b.addEventListener("click", validateForm);
+
+
+
+
+function validateForm(){
+let nombre = document.forms["myForm"]["name"].value;
+if (nombre == ""){
+    alert("El campo Nombre no puede estar vacío");
+}else {
+    let cadNombre = /^[A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+$/;
+    if (!cadNombre.test(nombre)){
+        alert("El Nombre solo puede contener letras, la primera mayúscula");
+    }
+}
 }
