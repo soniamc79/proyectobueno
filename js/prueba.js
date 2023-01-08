44c0899179e122933023757a0f2e8c32c3e7b702
soniamc79 <sonia.martinezcalleja@mengibar.eu>
2023-01-08 16:51:28 +0100

    formulario
    validacion

--- js/prueba.js
+++ js/prueba.js
@@ -9,12 +9,32 @@ const parrafo = document.getElementById("warnings");
 form.addEventListener("submit", e=>{
     e.preventDefault()
     let warnings = "";
-    let regexemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(nombre.Value.lenght <4){
-       warnings += "El nombre no es valido, tiene que tener cuatro caracteres como minimo"
+    let entrar = false;
+    let regexNombre = /^[A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+$/;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let regextlf = /^ (\+34|0034|34)?+(6|7)[0-9]{8}$/;
+    parrafo.innerHTML ="";
+    if(!regexNombre.test(nombre.value)){
+        warnings += "El nombre no es valido, tiene que tener cuatro caracteres como minimo"
+       entrar = true;
     }
     if(apellidos.value.lenght <4){
         warnings += "El apellido no es valido, tiene que tener cuatro caracteres como minimo"
+        entrar = true;
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += "El email no es valido"
+        entrar = true;
+    }
+    if(!regextlf.test(telefono.value)){
+        warning += "El telefono no es correcto";
+        entrar= true;
+    }
+
+    /*  */if(entrar){
+        parrafo.innerHTML = warnings;
+    }else{
+        parrafo.innerHTML = "El formulario se ha enviado"
     }
 })
 
